eeacb969913604b152e810d709c7aa98cd57ff7f
nestorjhernandezm <nestor@steinwurf.com>
2017-02-08 17:02:51 +0100

    Changes to handle char min and max values as ASCII. Not including pointers for now

--- Exercise-2-Datatypes-info/src/data_types_info.cpp
+++ Exercise-2-Datatypes-info/src/data_types_info.cpp
@@ -1,17 +1,41 @@
 #include <typeinfo>  // typeid
 #include <limits>  // numeric_limits
+#include <vector>
+#include <string>
+#include <algorithm>  // find
 
-#include <iostream>  // cout, endl
+#include <iostream>
 
 using namespace std;
 
 template<class T> void print_data_info()
 {
     cout << endl;
-    cout << "Type: " << typeid(T).name() << endl;
+    string type_name = typeid(T).name();
+    cout << "Type: " << type_name << endl;
     cout << "Size: " << sizeof(T) << " bytes" << endl;
-    cout << "Minimum value: " << numeric_limits<T>::min() << endl;
-    cout << "Maximum value: " << numeric_limits<T>::max() << endl;
+
+    T min = numeric_limits<T>::min();
+    T max = numeric_limits<T>::max();
+
+    // Force the type conversion in declaration in case of char-like types
+    const bool is_char_type = (type_name == "c" || type_name == "h" ||
+        type_name == "a");
+
+    if(!is_char_type)
+    {
+        cout << "Minimum value: " << min << endl;
+        cout << "Maximum value: " << max << endl;
+    }
+    else
+    {
+        // char values could also be interpreted as integers through ASCII
+        int ascii_min = min;
+        int ascii_max = max;
+        cout << "Minimum value: " << ascii_min << endl;
+        cout << "Maximum value: " << ascii_max << endl;
+    }
+
     cout << "Type is signed: " << std::numeric_limits<T>::is_signed << endl;
     cout << "Non-sign bits in type: " << std::numeric_limits<T>::digits
          << endl;
@@ -33,6 +57,15 @@ int main()
     print_data_info<double>();
     print_data_info<float>();
     print_data_info<char>();
+    print_data_info<unsigned int>();
+    print_data_info<unsigned short>();
+    print_data_info<unsigned long>();
+    print_data_info<unsigned char>();
+    print_data_info<signed int>();
+    print_data_info<signed short>();
+    print_data_info<signed long>();
+    print_data_info<signed char>();
+    print_data_info<wchar_t>();
 
     return 0;
 }
