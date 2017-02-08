a28ba1a4feb29a9ee80bf32b5c7babe7edf560a1
nestorjhernandezm <nestor@steinwurf.com>
2017-02-08 23:06:28 +0100

    Set the first column as decimal

--- Exercise-3-Ascii-table/src/ascii_table.cpp
+++ Exercise-3-Ascii-table/src/ascii_table.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 int main()
 {
-    // Let's get the ASCII table of all printable characters
+    // Let's get the ASCII table of all basic printable characters
 
     cout << endl;
-    cout << "ASCII Printable Characters" << endl;
+    cout << "ASCII Basic Printable Characters" << endl;
     cout << endl;
     cout << "Decimal\t\tHexadecimal\tCharacter" << endl;
     cout << "-------\t\t-----------\t---------" << endl;
 
     for(int i = 33; i < 127; i++)
     {
-        cout << i << "\t\t0x" << hex << i << "\t\t" << static_cast<char>(i) <<
-            endl;
+        cout << dec << i;
+        cout << "\t\t0x" << hex << i;
+        cout << "\t\t" << static_cast<char>(i) << endl;
     }
 
     return 0;
