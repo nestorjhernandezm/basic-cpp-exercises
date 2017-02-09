7250e25c8830e88e2ad283c8300cd5b5de567355
nestorjhernandezm <nestor@steinwurf.com>
2017-02-09 20:03:48 +0100

    Adding second exercise of pointers

--- Exercise-4-Pointers-arrays-references-structures/src/pointer_basics_II.cpp
+++ Exercise-4-Pointers-arrays-references-structures/src/pointer_basics_II.cpp
@@ -4,9 +4,37 @@ using namespace std;
 
 int main()
 {
-    // 4.2)  find the max of an integral data set. The program will ask
-    // the user to input the number of data values in the set and each value.
+    // 4.2) Find the max of an integral data set. The program will ask the
+    // user to input the number of data values in the set and each value.
     // The program prints on screen a pointer that points to the max value.
+
+    int size;
+    cout << "Enter the number of elements in the data set: " << endl;
+    cin >> size;
+
+    int max = 0;
+    int* ptr_max = &max;
+
+    for (int i = 0; i < size; i++)
+    {
+        int value;
+        cout << "Enter value " << i + 1 << " of the dataset" << endl;
+        cin >> value;
+
+        if (!i)
+        {
+            max = value;
+        }
+        else
+        {
+            if (max < value)
+            {
+                max = value;
+            }
+        }
+    }
+
+    cout << "The maximum is: " << *ptr_max << endl;
 
     return 0;
 }
