5067eeba9d89b97843ba28b966f6cb01ee3ceedd
nestorjhernandezm <nestor@steinwurf.com>
2017-02-09 18:01:57 +0100

    Adding new exercises

---
+++ Exercise-4-Pointers-arrays-references-structures/src/pointer_basics_II.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 4.2)  find the max of an integral data set. The program will ask
+    // the user to input the number of data values in the set and each value.
+    // The program prints on screen a pointer that points to the max value.
+
+    return 0;
+}
