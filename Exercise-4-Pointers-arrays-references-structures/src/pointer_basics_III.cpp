bb065b40b8de0a7e0d2101c689da4959e511e554
nestorjhernandezm <nestor@steinwurf.com>
2017-02-09 20:11:28 +0100

    Exercise 4.3 with string literals

---
+++ Exercise-4-Pointers-arrays-references-structures/src/pointer_basics_III.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 4.3) Given the string "A string." Print on one line the letter on the
+    // index 0, the pointer position and the letter t. undate the pointer to
+    // pointer +2. Then, in another line print the pointer and the letters r
+    // and g of the string (using the pointer).
+
+    return 0;
+}
