7c03b34861e14d32aa5038abf63d422064bef52b
nestorjhernandezm <nestor@steinwurf.com>
2017-02-11 02:45:29 +0100

    Note regarding C deprecated features of this exercise and some grammar corrections to the problem statement

--- Exercise-4-Pointers-arrays-references-structures/src/pointer_basics_III.cpp
+++ Exercise-4-Pointers-arrays-references-structures/src/pointer_basics_III.cpp
@@ -5,20 +5,23 @@ using namespace std;
 int main()
 {
     // 4.3) Given the string "A string." Print on one line the letter on the
-    // index 0 (A), the pointer position and the letter at index 3 (t). Undate
-    // the pointer to pointer + 2. Then, in another line, print the pointer
-    // and the letters r and g of the string (using the pointer). Start with
-    // string literals and then string class
+    // index 0 by indexing the string, the pointer default pointed value
+    // and the letter t using the pointer. Update the pointer to pointer + 2.
+    // Then, in another line, print the new pointed value and the letters r
+    // and g of the string (using the pointer).
+
+    // NOTE: This exercise though instructive, considers a deprecated feature
+    // inherited from C
 
     char s[] = "A string.";
-    char* p = s;
+    char* p = s;  // A string literal is also a char* (in C, but deprecated)
 
-    cout << "Letter at index 0: " << *p << ". Pointer position: " <<
-        &p << ". Letter at index 3: " << *(p + 3) << endl;
-
+    cout << "Letter at index 0: " << s[0] << ", Default pointed value: " <<
+        *p << ", Letter at index 3: " << p[3] << endl;  // *(p + 3) also works
     p += 2;
 
-    cout << "Pointer position: " << &p << " " << *(p + 2) << *(p + 5) << endl;
+    cout << "New pointed value: " << *p << ", Letters: " << p[2] << p[5] <<
+        endl;  // *(p + 2) and *(p + 5) also work as well
 
     return 0;
 }
