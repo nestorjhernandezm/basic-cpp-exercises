1abd9d63b392afb76be66bb9d8ea3d446d4ec834
nestorjhernandezm <nestor@steinwurf.com>
2017-04-03 20:56:52 +0200

    Adding some more code, needs work

--- Exercise-4-Pointers-arrays-references-structures/src/date.cpp
+++ Exercise-4-Pointers-arrays-references-structures/src/date.cpp
@@ -2,11 +2,20 @@
 
 using namespace std;
 
+struct Date
+{
+    int day;
+    int month;
+    int year;
+};
+
 int main()
 {
     // 4.4) Define a struct called Date to keep track of dates. Provide
     // functions that reads a Date from input, writes a Date to output
     // and initializes a Date with a date.
+
+
 
     return 0;
 }
