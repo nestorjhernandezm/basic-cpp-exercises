70cc01df10d156812d02906e5a4708b2166b62c0
nestorjhernandezm <nestor@steinwurf.com>
2017-04-03 20:54:11 +0200

    Making a struct exercise

---
+++ Exercise-4-Pointers-arrays-references-structures/src/date.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 4.4) Define a struct called Date to keep track of dates. Provide
+    // functions that reads a Date from input, writes a Date to output
+    // and initializes a Date with a date.
+
+    return 0;
+}
