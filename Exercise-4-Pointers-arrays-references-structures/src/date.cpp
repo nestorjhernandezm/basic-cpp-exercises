7438b7453a4f44b14ef0c92f941d0fa1a6f459f6
nestorjhernandezm <nestor@steinwurf.com>
2017-04-12 17:28:01 +0200

    Working with the date example

--- Exercise-4-Pointers-arrays-references-structures/src/date.cpp
+++ Exercise-4-Pointers-arrays-references-structures/src/date.cpp
@@ -15,7 +15,14 @@ int main()
     // functions that reads a Date from input, writes a Date to output
     // and initializes a Date with a date.
 
+    Date today;
 
+    today.day = 12;
+    today.month = 4;
+    today.year = 2017;
+
+    cout << "Today is: " << today.day << " " << today.month << " "
+        << today.year << endl;
 
     return 0;
 }
